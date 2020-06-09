6f1d352d55c0e36628582d8198833bbcd2b5f4cb
Edison Garcia Chinas <edisga@microsoft.com>
2020-06-09 09:28:47 -0500

    Adding memory_profiler

---
+++ memory/memory_profiler_initial/app.py
@@ -0,0 +1,19 @@
+## Sample code taken from Official Library repository https://github.com/pythonprofilers/memory_profiler/blob/master/examples/numpy_example.py
+
+import numpy as np
+import scipy.signal
+
+def create_data():
+    ret = []
+    for n in range(50):
+        ret.append(np.random.randn(1, 70, 71, 72))
+    return ret
+
+def process_data(data):
+    data = np.concatenate(data)
+    detrended = scipy.signal.detrend(data, axis=0)
+    return detrended
+
+if __name__ == "__main__":
+    raw_data = create_data()
+    processed_data = process_data(raw_data)
